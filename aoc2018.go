22bba0d52dd41fbf1f1c860272eaa065c0c43e92
GaYoung Park <gayoung@tubularlabs.com>
2018-12-04 23:51:57 -0800

    ADD profiling

--- aoc2018.go
+++ aoc2018.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
+	"runtime/pprof"
 	"strconv"
 )
 
@@ -15,15 +17,39 @@ var funcs = []func(){
 
 func main() {
 	log.SetFlags(0)
-	if len(os.Args) < 2 {
-		log.Fatalf("Usage: %s <problem>", os.Args[0])
+
+	profileFilename := flag.String("cpuprofile", "", "write cpu profile to `file`")
+	flag.Parse()
+
+	_ = profileFilename
+
+	if len(flag.Args()) < 1 {
+		log.Fatalf("Usage: %s [flags] <problem>", os.Args[0])
 	}
-	n, err := strconv.Atoi(os.Args[1])
+	n, err := strconv.Atoi(flag.Arg(0))
 	if err != nil {
 		log.Fatalln("Bad problem number:", err)
 	}
 	if n < 1 || n > len(funcs) {
 		log.Fatalf("Problem number out of range: %d", n)
 	}
+
+	if *profileFilename != "" {
+		f, err := os.Create(*profileFilename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer func() {
+			if err := f.Close(); err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatalln("Cannot start profile:", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	funcs[n-1]()
 }
